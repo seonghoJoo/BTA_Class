9de5d32d269044aa338a0e768a2d5819e2cdc708
seonghJoo <etkl8888@naver.com>
2022-04-20 00:54:28 +0900

    Info Service 소스 코드 리팩토링

--- week2_info/src/main/java/com/example/week2_info/InfoHandler.java
+++ week2_info/src/main/java/com/example/week2_info/InfoHandler.java
@@ -1,16 +1,19 @@
 package com.example.week2_info;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+@Component
 public class InfoHandler {
 
     // 과제2
     public Mono<ServerResponse> info_service_uri(ServerRequest request) {
         String name = request.queryParam("name").get();
 
+        System.out.println(name);
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
                 .body(
                         Mono.just(new InfoServiceResponseDto(""))
