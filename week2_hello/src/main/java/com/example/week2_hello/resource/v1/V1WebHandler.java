c0c6f24bdc439643be249936968af8d55b15a24f
seonghJoo <etkl8888@naver.com>
2022-04-19 23:19:20 +0900

    주석추가

--- week2_hello/src/main/java/com/example/week2_hello/resource/v1/V1WebHandler.java
+++ week2_hello/src/main/java/com/example/week2_hello/resource/v1/V1WebHandler.java
@@ -1,25 +1,20 @@
-package com.example.week2_hello.resource;
+package com.example.week2_hello.resource.v1;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.RouterFunction;
+
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Component
-public class MyWebHandler {
+public class V1WebHandler {
 
-    //Router Function 모델
-
+    // Router Function 모델
+    // 과제 1
     public Mono<ServerResponse> hello(ServerRequest request) {
         String name = request.queryParam("name").get();
 
