45e17ff27c031fe412db35cb0cf42128303ebf49
seonghJoo <etkl8888@naver.com>
2022-04-21 10:03:04 +0900

    UPDATE WebClient Bean 등록 및 설정 초기화

--- week2_hello/src/main/java/com/example/week2_hello/WebClientConfig.java
+++ week2_hello/src/main/java/com/example/week2_hello/WebClientConfig.java
@@ -1,29 +1,19 @@
 package com.example.week2_hello;
 
-import io.netty.channel.ChannelOption;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.http.codec.LoggingCodecSupport;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
-@Slf4j
+@EnableWebFlux
 public class WebClientConfig {
 
-    private final WebClient client;
-
-    public WebClientConfig(WebClient.Builder builder) {
-        this.client = builder.baseUrl("http://localhost:8092").build();
+    @Bean
+    public WebClient webClient(){
+        return  WebClient.builder()
+                .baseUrl("http://localhost:8092")
+                .build();
     }
 
 }
