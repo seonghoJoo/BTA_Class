d1dcf5b22405b086fbbf993e08fca22398285519
seonghJoo <etkl8888@naver.com>
2022-04-21 10:01:54 +0900

    UPDATE NoArgs/Setter 추가

--- week2_hello/src/main/java/com/example/week2_hello/resource/v2/vo/resp/InfoResponseDto.java
+++ week2_hello/src/main/java/com/example/week2_hello/resource/v2/vo/resp/InfoResponseDto.java
@@ -2,9 +2,13 @@ package com.example.week2_hello.resource.v2.vo.resp;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Setter
 public class InfoResponseDto {
     private String job;
 }
--- week2_hello/src/main/java/com/example/week2_hello/resource/v2/vo/resp/PersonResponseDto.java
+++ week2_hello/src/main/java/com/example/week2_hello/resource/v2/vo/resp/PersonResponseDto.java
@@ -2,11 +2,13 @@ package com.example.week2_hello.resource.v2.vo.resp;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PersonResponseDto{
     private String to;
     private String message;
